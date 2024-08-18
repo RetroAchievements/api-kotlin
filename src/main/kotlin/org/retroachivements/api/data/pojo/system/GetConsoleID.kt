dd7f0842771e3008025d6514f5abfe92f4657f9c
タピオカ狐 <magneticchen@gmail.com>
2024-08-18 14:28:51 -0400

    Update GetConsoleID.kt

--- src/main/kotlin/org/retroachivements/api/data/pojo/system/GetConsoleID.kt
+++ src/main/kotlin/org/retroachivements/api/data/pojo/system/GetConsoleID.kt
@@ -7,13 +7,19 @@ class GetConsoleID {
     class Response: ArrayList<Response.Console>() {
         data class Console (
             @SerializedName("ID")
-            val id: String,
+            val id: Long,
 
             @SerializedName("Name")
             val name: String,
 
             @SerializedName("IconURL")
             val iconUrl: String
+
+            @SerializedName("Active")
+            val active: Boolean
+
+            @SerializedName("IsGameSystem")
+            val isGameSystem: Boolean
         )
     }
 }
