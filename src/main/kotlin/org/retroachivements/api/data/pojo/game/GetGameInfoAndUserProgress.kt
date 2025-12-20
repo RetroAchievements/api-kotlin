ee1601f1cb8a9af12b41aef5cea1dc202b400e65
wizard <lenkgit@gmail.com>
2025-12-20 13:57:58 -0500

    feat: add UserTotalPlaytime

--- src/main/kotlin/org/retroachivements/api/data/pojo/game/GetGameInfoAndUserProgress.kt
+++ src/main/kotlin/org/retroachivements/api/data/pojo/game/GetGameInfoAndUserProgress.kt
@@ -56,6 +56,9 @@ class GetGameInfoAndUserProgress {
         @SerializedName("PlayersTotal")
         val playersTotal: Int,
 
+        @SerializedName("UserTotalPlaytime")
+        val userTotalPlaytime: Int,
+
         @SerializedName("GuideURL")
         val guideUrl: String?,
 
