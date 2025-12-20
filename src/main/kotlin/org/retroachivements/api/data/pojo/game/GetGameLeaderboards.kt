0d8c701c8a1637484d3673f76410ac1ff8f899ee
wizard <lenkgit@gmail.com>
2025-12-20 15:02:39 -0500

    feat: add Author and AuthorULID

--- src/main/kotlin/org/retroachivements/api/data/pojo/game/GetGameLeaderboards.kt
+++ src/main/kotlin/org/retroachivements/api/data/pojo/game/GetGameLeaderboards.kt
@@ -3,11 +3,14 @@ package org.retroachivements.api.data.pojo.game
 import com.google.gson.annotations.SerializedName
 
 class GetGameLeaderboards {
+
     data class Response(
         @SerializedName("Count")
         val count: Long,
+
         @SerializedName("Total")
         val total: Long,
+
         @SerializedName("Results")
         val results: List<Leaderboard>,
     )
@@ -15,23 +18,39 @@ class GetGameLeaderboards {
     data class Leaderboard(
         @SerializedName("ID")
         val id: Long,
+
         @SerializedName("RankAsc")
         val rankAsc: Boolean,
+
         @SerializedName("Title")
         val title: String,
+
         @SerializedName("Description")
         val description: String,
+
         @SerializedName("Format")
         val format: String,
+
         @SerializedName("TopEntry")
         val topEntry: TopEntry,
+
+        @SerializedName("Author")
+        val author: String,
+
+        @SerializedName("AuthorULID")
+        val authorUlid: String
     )
 
     data class TopEntry(
         @SerializedName("User")
         val user: String,
+
+        @SerializedName("ULID")
+        val ulid: String,
+
         @SerializedName("Score")
         val score: Long,
+
         @SerializedName("FormattedScore")
         val formattedScore: String,
     )
